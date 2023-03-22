5d8fe9fa77118616ac829b1a183fcce530857e3d
moisesmedeiros7 <moisesmedeiros@live.com>
2023-03-22 16:19:47 -0300

    Final da aula "Validação".

--- src/main/java/me/dio/academia/digital/service/impl/MatriculaServiceImpl.java
+++ src/main/java/me/dio/academia/digital/service/impl/MatriculaServiceImpl.java
@@ -3,9 +3,11 @@ package me.dio.academia.digital.service.impl;
 import me.dio.academia.digital.entity.Matricula;
 import me.dio.academia.digital.entity.form.MatriculaForm;
 import me.dio.academia.digital.service.IMatriculaService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class MatriculaServiceImpl implements IMatriculaService {
     @Override
     public Matricula create(MatriculaForm form) {
